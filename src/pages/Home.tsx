eec6066f5e5da024ab48787c8b1faf90de8e102d
LeandroRibeiroo <leandroribeiro.sa1997@gmail.com>
2021-11-06 10:45:27 -0300

    feat(App): Implementation of add task, mark as done and delete task functionalities.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,30 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const data: Task = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    };
+
+    setTasks((prevState) => [...prevState, data]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updatedTasks = tasks.map((task) => ({ ...task }));
+
+    updatedTasks.find((task) => {
+      if (task.id === id) {
+        task.done = !task.done;
+      }
+    });
+
+    setTasks(updatedTasks);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const newList = tasks.filter((task) => task.id !== id);
+    setTasks(newList);
   }
 
   return (
@@ -26,18 +41,18 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#EBEBEB'
-  }
-})
+    backgroundColor: '#EBEBEB',
+  },
+});
